2a1b73209377040eb937b7459f64ea12145d9fad
aatamanirbhar <vikasdubey.ece23@jecrc.ac.in>
2026-05-22 04:30:29 +0000

    resetpassword

--- src/lib/account.js
+++ src/lib/account.js
@@ -498,7 +498,7 @@ export async function updatePassword(password) {
 export async function requestPasswordReset(email) {
   const cleanEmail = email.trim();
   const { error } = await supabase.auth.resetPasswordForEmail(cleanEmail, {
-    redirectTo: `${window.location.origin}/reset-password`,
+    redirectTo: `https://www.gamenights.live/reset-password`,
   });
 
   if (error) throw error;
